1e7a458a1f24532de0d22ff8f58ef63341909ca0
Aakanshad28 <aakanshadahekar2001@gmail.com>
2023-12-28 00:03:02 +0530

    update delete and fetch all method is added

--- src/main/java/com/qsp/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/qsp/employee_management_system/controller/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.qsp.employee_management_system.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +31,18 @@ public class EmployeeController {
     	
 		
 	}
+    @GetMapping("/fetchAll")
+    public List<Employee> findAllEmployee() {
+		return dao.findAllEmployee();
+	}
+    @DeleteMapping("/delete/{id}")
+    public Employee deleteEmployee(@PathVariable int id) {
+    	return dao.deleteEmployee(id);
+		
+	}
+    @PutMapping("/update")
+    public Employee updateEmployee(@RequestParam int id,@RequestBody Employee employee) {
+    	return dao.updateEmployee(id,employee);
+    }
 
 }
--- src/main/java/com/qsp/employee_management_system/dao/EmployeeDao.java
+++ src/main/java/com/qsp/employee_management_system/dao/EmployeeDao.java
@@ -1,9 +1,10 @@
 package com.qsp.employee_management_system.dao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.qsp.employee_management_system.dto.Employee;
 import com.qsp.employee_management_system.repo.EmployeeRepo;
 
@@ -11,14 +12,58 @@ import com.qsp.employee_management_system.repo.EmployeeRepo;
 public class EmployeeDao {
 	@Autowired
 	private EmployeeRepo repo;
+
 	public Employee saveEmployee(Employee employee) {
 		return repo.save(employee);
 
 	}
-	 public Employee findEmployee( int id) {
-		return repo.findById(id).get();
-	    	
+
+	public Employee findEmployee(int id) {
+		
+		//return repo.findById(id).get();
+		//to avoid no such element exception
+		
+		Optional<Employee> optional=repo.findById(id);
+		if (optional.isPresent()) {
+			if (optional.isEmpty()) {
+				return optional.get();
+				//return null
+			}
+			return null;
+			//return optional.get();
+		}
+		return null;
+
+	}
+
+	public List<Employee> findAllEmployee() {
+        return repo.findAll();
+        
+	}
+
+	public Employee deleteEmployee(int id) {
+		Optional<Employee> optional=repo.findById(id);
+		if (optional.isPresent()) {
+			//repo.deleteById(id);
+//			return optional.get();
+			
+			Employee employee=optional.get();
+			repo.delete(employee);
+			return employee;
 			
 		}
+		return null;
+	}
+
+	public Employee updateEmployee(int id, Employee employee) {
+		Optional<Employee> optional=repo.findById(id);
+		if (optional.isPresent()) {
+			employee.setId(id);
+			return repo.save(employee);
+		}
+		return null;
+	}
+	
+	
 
 }
